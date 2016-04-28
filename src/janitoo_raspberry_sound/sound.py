f71fa00a3dc38202b03dea375131c0cb5c593ef3
bibi21000 <bibi21000@gmail.com>
2016-04-28 23:26:35 +0200

    Initial import

---
+++ src/janitoo_raspberry_sound/sound.py
@@ -0,0 +1,114 @@
+# -*- coding: utf-8 -*-
+"""The Raspberry GPIO components
+
+"""
+
+__license__ = """
+    This file is part of Janitoo.
+
+    Janitoo is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Janitoo is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Janitoo. If not, see <http://www.gnu.org/licenses/>.
+
+"""
+__author__ = 'Sébastien GALLET aka bibi21000'
+__email__ = 'bibi21000@gmail.com'
+__copyright__ = "Copyright © 2013-2014-2015-2016 Sébastien GALLET aka bibi21000"
+
+import logging
+logger = logging.getLogger(__name__)
+import os, sys
+import time
+import datetime
+import threading
+
+from janitoo.thread import JNTBusThread, BaseThread
+from janitoo.options import get_option_autostart
+from janitoo.utils import HADD
+from janitoo.node import JNTNode
+from janitoo.value import JNTValue
+from janitoo.component import JNTComponent
+from janitoo.bus import JNTBus
+try:
+    import Adafruit_GPIO as GPIO
+except:
+    logger.exception('Can"t import GPIO')
+
+##############################################################
+#Check that we are in sync with the official command classes
+#Must be implemented for non-regression
+from janitoo.classes import COMMAND_DESC
+
+COMMAND_SWITCH_BINARY = 0x0025
+COMMAND_WEB_RESOURCE = 0x1031
+COMMAND_DOC_RESOURCE = 0x1032
+
+assert(COMMAND_DESC[COMMAND_SWITCH_BINARY] == 'COMMAND_SWITCH_BINARY')
+assert(COMMAND_DESC[COMMAND_WEB_RESOURCE] == 'COMMAND_WEB_RESOURCE')
+assert(COMMAND_DESC[COMMAND_DOC_RESOURCE] == 'COMMAND_DOC_RESOURCE')
+##############################################################
+
+def make_input(**kwargs):
+    return InputComponent(**kwargs)
+
+class SoundBus(JNTBus):
+    """A bus to manage GPIO
+    """
+    def __init__(self, **kwargs):
+        """
+        :param kwargs: parameters transmitted to :py:class:`smbus.SMBus` initializer
+        """
+        JNTBus.__init__(self, **kwargs)
+        self._lock =  threading.Lock()
+        self.sound_device = None
+        self.export_attrs('sound_device', self.sound_device)
+
+    def check_heartbeat(self):
+        """Check that the component is 'available'
+
+        """
+        return self.sound_device is not None
+
+    def start(self, mqttc, trigger_thread_reload_cb=None):
+        """Start the bus
+        """
+        JNTBus.start(self, mqttc, trigger_thread_reload_cb)
+        try:
+            self.sound_device = None
+        except:
+            logger.exception("[%s] - Exception when starting sound bus", self.__class__.__name__)
+        self.update_attrs('sound_device', self.sound_device)
+
+    def stop(self):
+        """Stop the bus
+        """
+        JNTBus.stop(self)
+        try:
+            pass
+        except:
+            logger.exception("[%s] - Exception when stopping sound bus", self.__class__.__name__)
+        self.sound_device = None
+        self.update_attrs('sound_device', self.sound_device)
+
+class InputComponent(JNTComponent):
+    """ A generic component for gpio """
+
+    def __init__(self, **kwargs):
+        """
+        """
+        oid = kwargs.pop('oid', 'rpisound.input')
+        name = kwargs.pop('name', "Input")
+        product_name = kwargs.pop('product_name', "Audio")
+        product_type = kwargs.pop('product_type', "Software")
+        product_manufacturer = kwargs.pop('product_manufacturer', "Janitoo")
+        JNTComponent.__init__(self, oid=oid, name=name, product_name=product_name, product_type=product_type, product_manufacturer=product_manufacturer, **kwargs)
+        logger.debug("[%s] - __init__ node uuid:%s", self.__class__.__name__, self.uuid)
